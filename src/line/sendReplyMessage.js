b23a135f9a75e4b517d94d102a2e764e99452f1f
nnjyami <y@179.jp>
2020-05-09 14:49:40 +0900

    [Fix] 引数名を変更

--- src/line/sendReplyMessage.js
+++ src/line/sendReplyMessage.js
@@ -1,13 +1,13 @@
 const line = require("@line/bot-sdk");
 
-export default function (replyToken, message) {
+export default function (replyToken, text) {
   const client = new line.Client({
     channelAccessToken: process.env.LINE_CHANEL_ACCESS_TOKEN,
   });
 
   const message = {
     type: "text",
-    text: `下記のメッセージありがとう！\n${message}`,
+    text: `下記のメッセージありがとう！\n${text}`,
   };
 
   client
