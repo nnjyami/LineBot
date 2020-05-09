b180a0dcba66df6678a823daead8af6372398f5d
nnjyami <y@179.jp>
2020-05-09 16:18:17 +0900

    [Add] エラー処理を追加

--- src/reply.js
+++ src/reply.js
@@ -2,20 +2,26 @@ import { isLineSignature } from "./line/xLineSignature";
 import sendReplyMessage from "./line/sendReplyMessage";
 
 exports.handler = async (event, context) => {
+  let isError = false;
   console.log("event", event);
   console.log("context", context);
   if (!isLineSignature(event.body, event.headers["x-line-signature"])) {
-    return {
-      statusCode: 404,
-      body: "Something wrong",
-    };
+    isError = true;
   }
   const events = event.body.events;
+  console.log(event.body.events);
+  if (!events) isError = true;
   events.forEach((e) => {
     if (e.type === "message") {
       sendReplyMessage(e.replyToken, e.message.text);
     }
   });
+  if (isError) {
+    return {
+      statusCode: 404,
+      body: "Something wrong",
+    };
+  }
   return {
     statusCode: 200,
     body: "Hello World",
