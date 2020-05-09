ff3ede56831458f80107e4e838e0c28d9fe909c7
nnjyami <y@179.jp>
2020-05-09 16:11:25 +0900

    [Fix] LineSignature の引数を修正

--- src/hello.js
+++ src/hello.js
@@ -3,7 +3,7 @@ import { isLineSignature } from "./line/xLineSignature";
 exports.handler = async (event, context) => {
   console.log("event", event);
   console.log("context", context);
-  if (!isLineSignature(event.headers["x-line-signature"])) {
+  if (!isLineSignature(event.body, event.headers["x-line-signature"])) {
     return {
       statusCode: 404,
       body: "Something wrong",
--- src/reply.js
+++ src/reply.js
@@ -4,7 +4,7 @@ import sendReplyMessage from "./line/sendReplyMessage";
 exports.handler = async (event, context) => {
   console.log("event", event);
   console.log("context", context);
-  if (!isLineSignature(event.headers["x-line-signature"])) {
+  if (!isLineSignature(event.body, event.headers["x-line-signature"])) {
     return {
       statusCode: 404,
       body: "Something wrong",
