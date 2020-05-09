f92b5d270af0e2dd0884c4ebd740a3676e44b316
nnjyami <y@179.jp>
2020-05-09 21:50:12 +0900

    [Fix] Reply 処理修正

--- src/line/sendReplyMessage.js
+++ src/line/sendReplyMessage.js
@@ -1,28 +1,26 @@
 const line = require("@line/bot-sdk");
 
 export default async function (replyToken, text) {
-  return new Promise((resolve, reject) => {
-    const client = new line.Client({
-      channelSecret: process.env.LINE_CHANNEL_SECRET,
-      channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    });
+  const client = new line.Client({
+    channelSecret: process.env.LINE_CHANNEL_SECRET,
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  });
 
-    if (!client) console.log("clientError", client);
+  if (!client) console.log("clientError", client);
 
-    const message = {
-      type: "text",
-      text: `下記のメッセージありがとう！${text}`,
-    };
+  const message = {
+    type: "text",
+    text: `下記のメッセージありがとう！${text}`,
+  };
 
-    console.log(replyToken, text);
-    const response = client.replyMessage(replyToken, message).catch((error) => {
+  console.log(replyToken, text);
+  client
+    .replyMessage(replyToken, message)
+    .then(() => {
+      console.log("Sucessr");
+    })
+    .catch((error) => {
       // error handling
       console.log("send Message Error", error);
-      reject();
     });
-    if (response) {
-      console.log("success", response);
-      resolve();
-    }
-  });
 }
